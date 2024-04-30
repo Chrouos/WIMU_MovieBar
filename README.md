6b3e110082ae0e5dfdfd428ee596b9d49240520e
Huai <chrbezz0487@gmail.com>
2024-04-30 12:57:22 +0800

    ✨ feat(增加 web search 上網):

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # WIMU_MovieBar
+
+# Setting
+create a new file name `.env` to save the default settings
+```
+EMAIL="change to your email address"
+PASSWD="change to your password"
+cookie_path_dir="./cookies/"
+```
--- connect_chat.py
+++ connect_chat.py
@@ -7,6 +7,7 @@ from hugchat.login import Login
 
 class HuggingChatMovie:
     def __init__(self) -> None:
+        
         # 載入環境變數
         load_dotenv()
 
@@ -14,6 +15,10 @@ class HuggingChatMovie:
         self.EMAIL = os.getenv('EMAIL')
         self.PASSWD = os.getenv('PASSWD')
         self.cookie_path_dir = os.getenv('cookie_path_dir')
+        self.chatbot = None
+        
+        #- Login & Output Information.
+        self.login()
         print(textwrap.dedent("""
         Logging Information: 
         - EMAIL: {EMAIL},
@@ -21,23 +26,37 @@ class HuggingChatMovie:
         －－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
         """).format(EMAIL=self.EMAIL, PASSWD=self.PASSWD, cookie_path_dir=self.cookie_path_dir))
         
-        # 變數
-        self.chatbot = None
-        
     def login(self):
         # 登入
         sign = Login(self.EMAIL, self.PASSWD)
         cookies = sign.login(cookie_dir_path=self.cookie_path_dir, save_cookies=True)
         self.chatbot = hugchat.ChatBot(cookies=cookies.get_dict()) 
         
-    def query(self, query):
-        query_result = self.chatbot.chat(query)
-        print(query_result) # or query_result.text or query_result["text"]
+    def query(self, _query, _web_search=False):
+        
+        query_result = self.chatbot.query(_query, web_search=_web_search)
+        print(query_result)
+        for source in query_result.web_search_sources:
+            print(source.link)
+            print(source.title)
+            print(source.hostname)
+            
+    def change_assistant(self, _assistant_name):
+        try:
+            assistant = self.chatbot.search_assistant(assistant_name=_assistant_name)
+            self.chatbot.new_conversation(assistant=assistant, switch_to=True)
+            print(f"Now you assigned assistant: {assistant}")
+        except Exception as e:
+            print(f"[change_assistant] error in: {e}")
         
     
 if __name__ == "__main__":
     
     hugging_chat_movie = HuggingChatMovie()
-    hugging_chat_movie.login()
-    hugging_chat_movie.query("告訴我 [overlord] 這部動漫在說什麼")
+    
+    hugging_chat_movie.change_assistant(_assistant_name="Movie and TV Recommendations")
+    hugging_chat_movie.query(
+        _query="告訴我 [overlord] 這部動漫在說什麼", 
+        _web_search=True
+    )
     
